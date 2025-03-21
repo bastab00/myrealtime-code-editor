78dd1c093b553ff9868826669738a60e07fa2966
bastab00 <143991112+bastab00@users.noreply.github.com>
2025-03-21 16:09:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -63,5 +63,6 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
